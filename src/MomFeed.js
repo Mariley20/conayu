2f8f2c7b713d7d425b0530d563562175b0a2a19f
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-10-14 21:38:52 -0500

    imagen bebe

--- src/MomFeed.js
+++ src/MomFeed.js
@@ -9,7 +9,16 @@ class MomFeed extends Component {
 			}
 		}
 		render () {
-			const {model} = this.props;
+      const {model} = this.props;
+
+      var images =  [];
+
+      for(var i=1; i<=24; i++){
+        images.push(
+          <img src={"/images/" + i + "b.PNG"} />
+        )
+      }
+
     return (
         <div id="accordion" role="tablist">
         <div className="card">
@@ -51,6 +60,9 @@ class MomFeed extends Component {
           </div>
           <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
             <div className="card-body">
+
+              {images}
+
               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
             </div>
           </div>
