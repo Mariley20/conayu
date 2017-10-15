9d526f590ca23f9272548c0c8d5960b5cd5643b7
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-10-14 21:50:46 -0500

    momfeed

--- src/MomFeed.js
+++ src/MomFeed.js
@@ -25,7 +25,7 @@ class MomFeed extends Component {
           <div className="card-header" role="tab" id="headingOne">
             <h5 className="mb-0">
               <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Collapsible Group Item #1
+                Dale comidas espesas 5 veces al día
               </a>
             </h5>
           </div>
@@ -60,11 +60,22 @@ class MomFeed extends Component {
           </div>
           <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
             <div className="card-body">
-
-              {images}
-
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
+              <img src={"/images/" + "1" + ".PNG"} />
+              </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header" role="tab" id="headingThree">
+            <h5 className="mb-0">
+              <a className="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Collapsible Group Item #3
+              </a>
+            </h5>
+          </div>
+          <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+            <div className="card-body">
+              <img src={"/images/" + "1" + ".PNG"} />
+              </div>
           </div>
         </div>
       </div>
