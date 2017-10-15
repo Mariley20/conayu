64d4ebb7fcb308d7a3212619d0fed8d8cdb46ff5
Mariley20 <mariley2018@gmail.com>
2017-10-14 19:22:28 -0500

    HeaderMap

--- src/HeaderMap.js
+++ src/HeaderMap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import GoogleMaps from './GoogleMaps';
 import usuario from './img/usuario.png';
-import carro from './img/carro.png';
 import './App.css';
 import './Map.css';
 
@@ -18,7 +18,6 @@ class HeaderMap extends Component {
 			show: false
 		}
 	}
-
 	render() {
 		const { model } = this.props;
 		const open = () => {
@@ -31,10 +30,12 @@ class HeaderMap extends Component {
 				show: false
 			})
 		}
+		const onPathBntClick = () => {
+		model.setFilter(10);
+		}
 		return (
 			<header id="mapa_header">
 				<img className="img-responsive" id='usuario_logo' onClick={open} src={usuario} alt="" />
-				
 				<span className="fa-stack fa-lg fa-2x">
 					<i className="fa fa-circle fa-stack-2x"></i>
 					<i className="fa fa-gift fa-stack-1x fa-inverse"></i>
@@ -42,10 +43,6 @@ class HeaderMap extends Component {
 				<div id="mySidenav" className="sidenav" style={{ width: this.state.show ? '250px' : 0 }}>
 					<a href="javascript:void(0)" className="closebtn" onClick={close}>&times;</a>
 					<div id="datos_usuario">
-						{/* <div><strong>Name: </strong>{model.userInfo.name}</div>
-						<div><strong>Surname: </strong>{model.userInfo.surname}</div>
-						<div><strong>Phone: </strong>{model.userInfo.phone}</div>
-						<div><strong>Email: </strong>{model.userInfo.email}</div> */}
 					</div>
 				</div>
 			</header>
