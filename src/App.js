f6bf0fd09088b1a99a0b1e6e2b7f10803f5dcf94
Mariley20 <mariley2018@gmail.com>
2017-10-17 12:19:32 -0500

    gh-pages - app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-	BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
+    BrowserRouter,
+    Route,
+    Switch,
+    NavLink,
+    Redirect
 } from 'react-router-dom'
 import './App.css';
 
@@ -15,23 +15,21 @@ import MomFeed from './MomFeed';
 import BabyFeed from './BabyFeed';
 
 const App = (props) => {
-	const {model} =  props;
-	console.log('app.props', props)
-	return (<BrowserRouter>
-		<div>
-			<Switch>
-				<Route exact path="/"
-				       render={() => <Redirect to= {'/home'}/>}/>
-				<Route  path="/home" render={() => <Home model={model} />}/>
-				<Route  path="/signUp" render={() => <SignUp model={model} />}/>
-				<Route path="/map" render={() => <DataMap model={model}/>}/>
-				<Route  path="/momFeed" render={() => <MomFeed model={model} />}/>
-				<Route path="/babyFeed" render={() => <BabyFeed model={model}/>}/>
-				<Route path='/hackathon-midis' render={() => <Redirect to="/home"/>}/>
-			</Switch>
-		</div>
-	</BrowserRouter>)
+    const {model} =  props;
+    console.log('app.props', props)
+    return (<BrowserRouter>
+        <div>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to= {'/home'}/>}/>
+                <Route  path="/home" render={() => <Home model={model} />}/>
+                <Route  path="/signUp" render={() => <SignUp model={model} />}/>
+                <Route path="/map" render={() => <DataMap model={model}/>}/>
+                <Route  path="/momFeed" render={() => <MomFeed model={model} />}/>
+                <Route path="/babyFeed" render={() => <BabyFeed model={model}/>}/>
+                <Route path='/yanapa' render={() => <Redirect to="/home"/>}/>
+            </Switch>
+        </div>
+    </BrowserRouter>)
 }
 
 export default App;
-
